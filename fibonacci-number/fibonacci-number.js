dbd80545790ebaefb497bc23c8d7320872b78648
Simon Lu <simonlu1668@gmail.com>
2021-09-08 08:33:39 -0700

    Time: 709 ms (5.12%), Space: 44.6 MB (5.06%) - LeetHub

--- fibonacci-number/fibonacci-number.js
+++ fibonacci-number/fibonacci-number.js
@@ -3,14 +3,24 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n < 1) {
+    if(n === 0) {
         return 0;
     }
     
-    else if(n === 1) {
-        return 1;
-    } else {
-        return fib(n-1) + fib(n-2);
+    var innerFunction = (n, memo = {}) => {
+        
+        if(n in memo) return memo[n];
+        
+        if(n <= 2) {
+            return 1;
+        }
+        
+        memo[n] = innerFunction(n-1) + innerFunction(n-2);
+        return memo[n];
+        
     }
     
+    
+    var test = innerFunction(n);
+    return test;
 };
