e8ddff7a90ced0a1fcfb0631b16ea8ec05db5140
Simon Lu <simonlu1668@gmail.com>
2021-09-09 15:09:42 -0700

    Time: 80 ms (35.75%), Space: 40.7 MB (10.20%) - LeetHub

--- unique-paths/unique-paths.js
+++ unique-paths/unique-paths.js
@@ -5,18 +5,19 @@
  */
 var uniquePaths = function(m, n, memo = {}) {
     
-    var key = m + ',' + n;
+    let rows = m;
+    let columns = n; 
+    if (memo[rows + ',' + columns]) return memo[rows + ',' + columns];
 
-    if(key in memo) return memo[key]
-    if (n === 0 || m === 0) {
+    if (rows === 0 || columns === 0) {
         return 0;
     }
 
-    if (n === 1 && m === 1) {
+    if (rows === 1 && columns === 1) {
         return 1;
     }
 
-    memo[key] = uniquePaths(n-1,m, memo) + uniquePaths(n, m-1,memo);
+    memo[rows + ',' + columns] = uniquePaths(rows - 1, columns, memo) + uniquePaths(rows, columns - 1, memo);
 
-    return memo[key];
+    return memo[rows + ',' + columns];
 };
