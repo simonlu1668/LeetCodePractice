cbad5b18df292f72b2079c5ce02ee0bb54637b04
Simon Lu <simonlu1668@gmail.com>
2022-01-22 16:00:29 -0800

    Time: 188 ms (6.39%), Space: 38.4 MB (70.78%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n === 0) {
+        return 0;
+    }
+    
+    if(n === 1) {
+        return 1;
+    }
+    
+    if(n === 2) {
+        return 1;
+    }
+    
+    
+    return fib(n-1) + fib(n-2);
+};
