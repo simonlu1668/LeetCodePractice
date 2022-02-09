276c284a726b92e02e8fae9d885c81f81cd9dfdb
Simon Lu <simonlu1668@gmail.com>
2022-02-08 20:34:45 -0800

    Time: 91 ms (73.65%), Space: 44.9 MB (15.64%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -5,23 +5,22 @@
 var permute = function(nums) {
     let results = [];
     
-    const innerFunction = (currentArray) => {
-      if(currentArray.length === nums.length){
-          results.push(currentArray);
-          return;
-      }
-        
-        for(let element of nums){
-            if(!currentArray.includes(element)) {
-                innerFunction(currentArray.concat(element));
+    const innerFunction = (slate, size) => {
+        if(size == 0) {
+            results.push(slate);
+        } else {
+            for(let num of nums) {
+                if(!slate.includes(num)) {
+                    innerFunction(slate.concat(num), size-1);
+                }
             }
         }
     }
     
-    for(let i = 0; i < nums.length; i++) {
-           innerFunction([nums[i]]);
+    
+    for(let i = 0; i < nums.length; i++){
+        innerFunction([nums[i]], nums.length-1);
     }
- 
     
     return results;
 };
