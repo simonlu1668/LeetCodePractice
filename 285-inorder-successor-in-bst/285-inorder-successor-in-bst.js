b72c789fa3a0b6a334de2dfbc9273c4fb0a76c0d
Simon Lu <simonlu1668@gmail.com>
2022-02-16 19:14:07 -0800

    Time: 92 ms (88.38%), Space: 51.7 MB (35.31%) - LeetHub

---
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+    if(!root) {
+        return null;
+    }
+    
+    let current = p;
+    if(p.right){
+        current = p.right;
+        while(current.left) {
+            current = current.left;
+        }
+        return current;
+    } else {
+        let prev = null;
+        let current = root;
+        while(current.val !== p.val) {
+            if(current.val > p.val) {
+                prev = current;
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+       return prev;
+    }
+    
+};
