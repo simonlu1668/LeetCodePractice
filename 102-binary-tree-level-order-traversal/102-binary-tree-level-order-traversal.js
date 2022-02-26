38ca11387bb6cc85d1d745ef57d31f1f41da5799
Simon Lu <simonlu1668@gmail.com>
2022-02-25 17:40:35 -0800

    Time: 68 ms (92.82%), Space: 44.4 MB (27.96%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -1,7 +1,7 @@
 var levelOrder = function(root) {
     if (root == null) return [];
     let queue = [root], result = [];
-    while (queue.length > 0) {
+    while (queue.length) {
         let len = queue.length, level = [];
         for (let i = 0; i < len; i++) {
             let node = queue.shift();
