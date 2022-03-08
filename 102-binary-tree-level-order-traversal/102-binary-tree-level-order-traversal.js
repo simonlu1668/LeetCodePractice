c977c36cd59bbef13b1a2d43ca44199526ebd013
Simon Lu <simonlu1668@gmail.com>
2022-03-08 11:16:44 -0800

    Time: 98 ms (47.62%), Space: 44.5 MB (27.79%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.js
@@ -10,26 +10,30 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
-var levelOrder = function(root) {
-    let result = [];
-    if(!root) return result;
-    let queue = [root];
-    
-    const innerFunction = (node) => {
+var levelOrder = function (root) {
+    if(!root) return [];
+   let result = [];
+   let queue = [root];
 
-        while(queue.length > 0) {
-               let temp = [], queueLength = queue.length;
-            for(let i = 0; i < queueLength; i++) {
-                let current = queue.shift();
-                temp.push(current.val);
-                if(current.left) queue.push(current.left);
-                if(current.right) queue.push(current.right); 
-            }
-            result.push(temp);
-        }
-    }
-    
-    
-    innerFunction(root);
-    return result;
-}
+   while (queue.length) {
+      let temp = [];
+      let length = queue.length;
+      for (let i = 0; i < length; i++) {
+        let current = queue.shift();
+          if(current){
+              temp.push(current.val);
+          
+         
+         if (current.left) {
+            queue.push(current.left);
+         }
+         if (current.right) {
+            queue.push(current.right);
+         }
+          }
+      }
+       result.push(temp);
+      
+   }
+   return result;
+};
