bfbe9be94bd0acca45c2e4f8534a4eabc9751f87
Simon Lu <simonlu1668@gmail.com>
2022-03-10 21:14:55 -0800

    Time: 80 ms (62.44%), Space: 42 MB (47.05%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -3,27 +3,5 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
-    let hashTable = {};
-    
-    for(let i = 0; i < nums.length; i++) {
-        if(!hashTable[nums[i]]) hashTable[nums[i]] = 1
-    else hashTable[nums[i]]++
-    }
-    
-    for(let i = 0; i < nums.length; i++) {
-        if(hashTable[0] === 0) delete hashTable[0];
-        if(hashTable[1] === 0) delete hashTable[1];
-        if(hashTable[0]) {
-            nums[i] = 0;
-            hashTable[0]--;
-        } else if (hashTable[1]){
-            nums[i] = 1;
-            hashTable[1]--;
-        } else {
-            nums[i] = 2;
-        }
-
-    }
-    
-    console.log(hashTable);
+    return nums.sort((a,b)=>a-b);
 };
