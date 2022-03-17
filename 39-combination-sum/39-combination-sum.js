a7dc67b89b12dd2598347f8107aeb791e5dda800
Simon Lu <simonlu1668@gmail.com>
2022-03-17 16:56:37 -0700

    Time: 870 ms (5.01%), Space: 49 MB (23.67%) - LeetHub

--- 39-combination-sum/39-combination-sum.js
+++ 39-combination-sum/39-combination-sum.js
@@ -5,30 +5,27 @@
  */
 var combinationSum = function(candidates, target) {
     let result = [];
-    let hashmap = {};
+    let hash = {};
     
-    const innerFunction = (slate, current) => { 
-
-        if(current === 0) {
-            if(!hashmap[slate.sort((a,b)=>a-b)]) {
-                 result.push(slate);
-                hashmap[slate.sort((a,b)=>a-b)]=1;
+    const innerFunction = (newTarget = target, current = []) => {
+        if(newTarget < 0) return;
+        if(newTarget == 0){
+            let sorted = current.sort((a,b)=>a-b);
+            if(!hash[sorted]){
+                     result.push(current);
+                hash[sorted] = 1;
             }
-           
-            return;
+       
         }
         
-        if(current < 0) {
-            return;
-        }
-        
-        for(let candidate of candidates) {
-            innerFunction(slate.concat(candidate), current-candidate);
+        for(let i = 0; i < candidates.length; i++){
+            innerFunction(newTarget-candidates[i], current.concat(candidates[i]));
         }
     }
     
     
-    innerFunction([], target);
+    innerFunction();
+
     
     return result;
 };
