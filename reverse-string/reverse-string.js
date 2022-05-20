ea9574e24bd70c5f428d44a2d54c6e14ed5dbe59
Simon Lu <simonlu1668@gmail.com>
2022-05-19 17:10:10 -0700

    Time: 103 ms, Memory: 51.6 MB - LeetHub

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,24 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    if(!s){
+        return;
+    }
+    
+    const innerFunction = (left, right) => {
+        if(left >= right) return;
+        let temp = s[right];
+        s[right] = s[left];
+        s[left] = temp;
+        innerFunction(left+1, right-1);
+    }
+    
+    innerFunction(0, s.length-1);
+    
+    return s;
+    
+    
+    
+};
