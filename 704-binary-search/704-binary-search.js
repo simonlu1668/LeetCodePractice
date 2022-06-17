3752f7d7c9e70d2c06b9f3cb46d0adb3b6029c66
Simon Lu <simonlu1668@gmail.com>
2022-06-17 07:24:17 -0700

    Time: 72 ms (83.40%), Space: 44.9 MB (69.10%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,16 +5,16 @@
  */
 var search = function(nums, target) {
     let left = 0, right = nums.length-1;
-    
     while(left <= right){
         let mid = Math.floor((left+right)/2);
-        if(nums[mid] == target){
+        if(nums[mid] === target){
             return mid;
-        } else if (nums[mid] < target){
-            left = mid+1;
-        } else {
+        } else if (nums[mid] > target){
             right = mid-1;
+        } else {
+            left = mid+1;
         }
     }
+    
     return -1;
 };
