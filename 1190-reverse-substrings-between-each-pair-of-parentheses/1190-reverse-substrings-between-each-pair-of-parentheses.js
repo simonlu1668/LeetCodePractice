7ece43eaabfef90cf00375c91a16c8e5cc1caa4a
Simon Lu <simonlu1668@gmail.com>
2022-06-23 16:26:00 -0700

    Time: 97 ms (32.50%), Space: 41.9 MB (96.25%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.js
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.js
@@ -3,20 +3,21 @@
  * @return {string}
  */
 var reverseParentheses = function(s) {
-    if(s.length == 0) return "";
+    let result = "";
     let stack = [[]];
-   
     
     for(let i = 0; i < s.length; i++){
-        if(s[i] === '(') {
+        if(s[i] === '('){
             stack.push([]);
         } else if (s[i] === ')'){
-            let current = stack.pop().reverse();
-            stack[stack.length-1].push(...current);
+            let end = stack.pop();
+            end = end.reverse();
+            stack[stack.length-1].push(...end);
         } else {
             stack[stack.length-1].push(s[i]);
         }
     }
+    
     
     return stack[0].join('');
 };
