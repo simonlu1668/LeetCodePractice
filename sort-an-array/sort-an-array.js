7aedfb126fd8424476d9afda59e09ddd75acc1c6
Simon Lu <simonlu1668@gmail.com>
2022-06-24 06:36:11 -0700

    Time: 503 ms, Memory: 85.5 MB - LeetHub

---
+++ sort-an-array/sort-an-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+    //step one break the array into smallest form
+    let midPoint = Math.floor(nums.length/2);
+    let leftHalf = nums.slice(0,midPoint);
+    let rightHalf = nums.slice(midPoint);
+    const innerFunction = (currentArray) => {
+        if(currentArray.length === 1) return currentArray;
+        let midPoint = Math.floor(currentArray.length/2);
+        let leftHalf = innerFunction(currentArray.slice(0,midPoint));
+        let rightHalf = innerFunction(currentArray.slice(midPoint));
+        
+        return leftHalf.concat(rightHalf).sort((a,b)=> a-b);
+    }
+    
+    return innerFunction(nums);
+    
+    
+};
